6486b8f425901d880150f2ebc0c4c9fb7d852007
feyroozecode <ibrahmad18@gmail.com>
2025-03-08 11:40:09 +0100

    impl riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'src/core/presentation/app_widget.dart';
 
-void main (){
-  runApp(const AppWidget());
+void main() {
+  runApp(
+    ProviderScope(
+      child: const AppWidget(),
+    ),
+  );
 }
